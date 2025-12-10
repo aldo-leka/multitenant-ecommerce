c6aa40424f759e98ac167bd11f8621a72810488b
Aldo Leka <aldo.leka@live.com>
2025-12-10 17:46:54 +0100

    06: categories finalization

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -4,6 +4,7 @@ import { Navbar } from "./navbar";
 import { SearchFilters } from "./search-filters";
 import configPromise from "@payload-config";
 import { getPayload } from "payload";
+import { CustomCategory } from "./types";
 
 interface Props {
     children: React.ReactNode;
@@ -23,9 +24,10 @@ const Layout = async ({ children }: Props) => {
                 exists: false,
             },
         },
+        sort: "name",
     });
 
-    const formattedData = data.docs.map((doc) => ({
+    const formattedData: CustomCategory[] = data.docs.map((doc) => ({
         ...doc,
         subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
             // Because of 'depth: 1' we are confident "doc" will be of type "Category"
@@ -33,11 +35,6 @@ const Layout = async ({ children }: Props) => {
             subcategories: undefined,
         })),
     }));
-
-    console.log({
-        data,
-        formattedData,
-    });
 
     return (
         <div className="flex flex-col min-h-screen">
