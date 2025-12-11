30d9987dfc6dc3fb8b485c5850bd3dfc9fa064b4
Aldo Leka <aldo.leka@live.com>
2025-12-11 13:56:01 +0100

    08: authentication

--- src/app/(app)/(home)/navbar.tsx
+++ src/app/(app)/(home)/navbar.tsx
@@ -86,6 +86,7 @@ export const Navbar = () => {
                     className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg"
                 >
                     <Link
+                        prefetch
                         href="/sign-in"
                     >
                         Log in
@@ -96,6 +97,7 @@ export const Navbar = () => {
                     className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black transition-colors text-lg"
                 >
                     <Link
+                        prefetch
                         href="/sign-up"
                     >
                         Start selling
