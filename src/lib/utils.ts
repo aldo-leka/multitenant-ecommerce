9901d34b2b5b3309b6b0cb9e7fe05e907efed5d0
Aldo Leka <aldo.leka@live.com>
2025-12-13 18:36:07 +0100

    17: product page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,8 +3,16 @@ import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
+};
 
 export function generateTenantURL(tenantSlug: string) {
   return `/tenants/${tenantSlug}`;
+};
+
+export function formatCurrency(value: number | string) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    maximumFractionDigits: 0,
+  }).format(Number(value));
 }
